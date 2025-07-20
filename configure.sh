57b85687661241ec9d80b9e7f95d42357ad91952
Jon Fingas <jon.fingas@gmail.com>
2025-07-19 22:34:17 -0500

    building with sdl_image

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-cmake -S . -B build/
+cmake -DBUILD_SHARED_LIBS=OFF -DGLM_BUILD_TESTS=OFF -S . -B build/
--- src/SDL3-App.cpp
+++ src/SDL3-App.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3/SDL.h>
+#include <glm/glm.hpp>
 
 int main() {
+	glm::vec2 test = glm::vec2(2,2);
+	SDL_Window* window;
+	SDL_Texture* tex = IMG_LoadTexture(nullptr,"data/idle.png");
+	SDL_Log("Working.");
 	return 0;
 }
