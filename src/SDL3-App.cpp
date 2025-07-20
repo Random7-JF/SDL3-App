ed8cafd7db66f037120e515a2e64bb11ce82d4f8
Jon Fingas <jon.fingas@gmail.com>
2025-07-19 22:47:22 -0500

    Working and clean setup

--- src/SDL3-App.cpp
+++ src/SDL3-App.cpp
@@ -3,10 +3,9 @@
 #include <SDL3/SDL.h>
 #include <glm/glm.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
 	glm::vec2 test = glm::vec2(2,2);
 	SDL_Window* window;
 	SDL_Texture* tex = IMG_LoadTexture(nullptr,"data/idle.png");
 	SDL_Log("Working.");
-	return 0;
 }
