2988f324e05146a9b829b34b1a065fe39de3163c
Jon Fingas <jon.fingas@gmail.com>
2025-07-22 18:42:39 -0500

    working opengl

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -15,16 +15,17 @@ bool Game::Init()
   }
 
   m_state.window =
-      SDL_CreateWindow("SDL", m_state.windowWidth, m_state.windowHeight, SDL_WINDOW_RESIZABLE);
+      SDL_CreateWindow("SDL", m_state.windowWidth, m_state.windowHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
   if (!m_state.window)
   {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
                              "Error creating window", m_state.window);
     return initialized;
-  }
+  } 
   
   m_state.glcontext = SDL_GL_CreateContext(m_state.window);
   if (!m_state.glcontext) {
+    SDL_Log(SDL_GetError());
       SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
                            "Error creating gl context", m_state.window);
       return initialized;
@@ -39,6 +40,8 @@ bool Game::Init()
       SDL_Log("%s", glewError);
       return initialized;
   }
+  glViewport(0, 0, m_state.windowWidth, m_state.windowHeight); // Initial viewport
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Dark teal background
 
   // m_state.renderer = SDL_CreateRenderer(m_state.window, NULL);
   // if (!m_state.renderer)
@@ -82,13 +85,12 @@ void Game::Run()
       }
     } // end of event loop
 
-    // OpenGL
-    glViewport(0, 0, m_state.windowWidth, m_state.windowHeight); // Initial viewport
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Dark teal background
+  // 1. Clear the screen (using OpenGL function)
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
 
+  
+  SDL_GL_SwapWindow(m_state.window);
 
-    // swap buffers
-    SDL_GL_SwapWindow(m_state.window);
   } // end of running loop
   SDL_DestroyRenderer(m_state.renderer);
   SDL_DestroyWindow(m_state.window);
