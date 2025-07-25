4e7a6654d900a9499350ed76b0cafd60034c288d
Jon Fingas <jon.fingas@gmail.com>
2025-07-25 12:32:37 -0500

    fixed warnings in file.

--- src/game/asset.h
+++ src/game/asset.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL3_image/SDL_image.h>
+#include <SDL3/SDL.h>
 
 enum class AssetType
 {
@@ -11,11 +12,11 @@ class Asset
 {
 private:
 	AssetType m_type;
-	SDL_Texture *m_texture;
+	SDL_Texture* m_texture;
 	SDL_ScaleMode m_scaleMode;
 
 public:
-	Asset(SDL_Texture *texture, SDL_ScaleMode scaleMode) : m_texture(texture), m_type(AssetType::Texture), m_scaleMode(scaleMode)
+	Asset(SDL_Texture *texture, SDL_ScaleMode scaleMode) : m_type(AssetType::Texture), m_texture(texture), m_scaleMode(scaleMode)
 	{
 		SDL_SetTextureScaleMode(m_texture, scaleMode);
 	}
@@ -26,6 +27,10 @@ public:
 		case AssetType::Texture:
 		{
 			SDL_DestroyTexture(m_texture);
+			break;
+		}
+		case AssetType::Audio:
+		{
 			break;
 		}
 		}
